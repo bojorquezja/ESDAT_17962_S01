4ca46b3fdf08473738d7d0d9bc801325e51a8011
bojorquezja <bojorquez.ja@gmail.com>
2025-04-03 17:40:39 -0500

    nuevos metodos

--- listaenlazadaS/Locomotora.cs
+++ listaenlazadaS/Locomotora.cs
@@ -4,14 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace listaenlazadaS
-{
-    class Locomotora
-    {
+namespace listaenlazadaS{
+    class Locomotora{
         Vagon Primero { get; set; }
 
-        public void AgregaPrimero(int valor)
-        {
+        public void AgregaPrimero(int valor){
             Vagon nuevo = new Vagon(valor);
             if (this.Primero == null)//caso 1
             {
@@ -30,8 +27,7 @@ namespace listaenlazadaS
         }
 
 
-        public void AgregaFinal(int valor)
-        {
+        public void AgregaFinal(int valor){
             Vagon nuevo = new Vagon(valor);
             if (this.Primero == null)//caso 1
             {
@@ -47,8 +43,53 @@ namespace listaenlazadaS
                 tmp.Sig = nuevo;
             }
         }
-        public string VerVagones()
-        {
+        public void EliminaUltimo(){
+            if (this.Primero == null) // Caso 1: La lista está vacía
+            {
+                return;
+            }
+
+            if (this.Primero.Sig == null) // Caso 2: Solo hay un vagón
+            {
+                this.Primero = null;
+                return;
+            }
+
+            Vagon tmp = this.Primero;
+            while (tmp.Sig.Sig != null) // Recorremos hasta el penúltimo vagón
+            {
+                tmp = tmp.Sig;
+            }
+            tmp.Sig = null; // Eliminamos el último vagón
+        }
+
+        public int Largo() {
+            return 0;
+        }
+
+        public int GetValor(int pos) {
+            return 0;
+        }
+
+        public void AgregaLEFinal(Locomotora otro) {
+
+        }
+
+        public void Ordena() {
+
+        }
+
+        public bool ExisteValor(int valor) {
+            Vagon tmp = this.Primero;
+            while (tmp != null) {
+                if ( tmp.Dato == valor) {
+                    return true;
+                }
+                tmp = tmp.Sig;
+            }
+            return false;
+        }
+        public string VerVagones(){
             string listaVag = "";
             Vagon tmp = this.Primero;
             while (tmp != null) {
--- listaenlazadaS/Program.cs
+++ listaenlazadaS/Program.cs
@@ -6,3 +6,11 @@ l.AgregaFinal(2);
 l.AgregaFinal(10);
 l.AgregaFinal(20);
 Console.WriteLine(l.VerVagones());
+Console.WriteLine();
+l.EliminaUltimo();
+Console.WriteLine(l.VerVagones());
+if (l.ExisteValor(10)) {
+    Console.WriteLine("Existe el 10");
+} else {
+    Console.WriteLine("No Existe");
+}
